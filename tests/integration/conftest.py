53146f4c85d36fddef9304c5c3a22003f1586522
MJ Ponsonby <mj.ponsonby@canonical.com>
2025-07-24 12:53:17 +0100

    Get concierge to also set up microk8s
    allows for integration testing with k8s charms
    
    Signed-off-by: MJ Ponsonby <mj.ponsonby@canonical.com>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,8 +1,12 @@
+import contextlib
 import functools
+import secrets
 import time
+from typing import Generator
 
 import jubilant
 import pytest
+
 
 
 def pytest_addoption(parser: pytest.OptionGroup):
@@ -13,6 +17,32 @@ def pytest_addoption(parser: pytest.OptionGroup):
         help="Keep temporarily created models.",
     )
 
+# NOTE: workaround for temp_models with non default cloud in jubilant, to be
+# removed when upstream pr is closed
+@contextlib.contextmanager
+def temp_model(keep: bool = False,
+               controller: str | None = None,
+               cloud: str | None = None) -> Generator[jubilant.Juju, None, None]:
+    """Context manager to create a temporary model for running tests in.
+
+    This creates a new model with a random name in the format ``jubilant-abcd1234``, and destroys
+    it and its storage when the context manager exits.
+
+    Provides a :class:`Juju` instance to operate on.
+
+    Args:
+        keep: If true, keep the created model around when the context manager exits.
+        controller: Name of controller where the temporary model will be added.
+        cloud: Name of cloud where the temporary model will be added.
+    """
+    juju = jubilant.Juju()
+    model = 'jubilant-' + secrets.token_hex(4)  # 4 bytes (8 hex digits) should be plenty
+    juju.add_model(model, controller=controller,cloud=cloud)
+    try:
+        yield juju
+    finally:
+        if not keep:
+            juju.destroy_model(model, destroy_storage=True, force=True)
 
 @pytest.fixture
 def juju(request: pytest.FixtureRequest):
@@ -21,7 +51,7 @@ def juju(request: pytest.FixtureRequest):
     #      error when running in parallel (LP: #2053270).
     while (retry_count := 3):
         try:
-            with jubilant.temp_model(keep=keep_models) as juju:
+            with temp_model(keep=keep_models,cloud="localhost") as juju:
                 juju.wait_timeout = 15 * 60
                 juju.wait = functools.partial(juju.wait,
                                               error=jubilant.any_error)
