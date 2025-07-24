0d516d601920764ef16ff4c51acb967dfadd562f
MJ Ponsonby <mj.ponsonby@canonical.com>
2025-07-24 14:00:21 +0100

    Format files to standard formatting
    charm.py, conftest.py and test_main.py reformatted according to ruff
    format requirements
    
    Signed-off-by: MJ Ponsonby <mj.ponsonby@canonical.com>

--- microovn/src/charm.py
+++ microovn/src/charm.py
@@ -24,13 +24,14 @@ CSR_ATTRIBUTES = CertificateRequestAttributes(
     is_ca=True,
 )
 
+
 def call_microovn_command(*args, stdin=None):
     result = subprocess.run(
         ["microovn", *args],
         stdout=subprocess.PIPE,
         stderr=subprocess.DEVNULL,
         input=stdin,
-        text=True
+        text=True,
     )
     return result.returncode, result.stdout
 
@@ -56,9 +57,7 @@ class MicroovnCharm(ops.CharmBase):
         )
 
         self.token_consumer = TokenConsumer(
-            charm=self,
-            relation_name=WORKER_RELATION,
-            command_name=["microovn", "cluster"]
+            charm=self, relation_name=WORKER_RELATION, command_name=["microovn", "cluster"]
         )
 
         self.ovsdbcms_requires = OVSDBCMSRequires(
@@ -67,52 +66,54 @@ class MicroovnCharm(ops.CharmBase):
         )
 
         framework.observe(self.on.install, self._on_install)
-        framework.observe(self.on[WORKER_RELATION].relation_changed,
-                            self._on_cluster_changed)
+        framework.observe(self.on[WORKER_RELATION].relation_changed, self._on_cluster_changed)
         framework.observe(self.on.update_status, self._update_status)
 
         framework.observe(
             self.certificates.on.certificate_available, self._on_certificates_available
         )
-        framework.observe(
-            self.ovsdbcms_requires.on.ready, self._on_ovsdbcms_ready
-        )
-        framework.observe(
-            self.ovsdbcms_requires.on.goneaway , self._on_ovsdbcms_broken
-        )
+        framework.observe(self.ovsdbcms_requires.on.ready, self._on_ovsdbcms_ready)
+        framework.observe(self.ovsdbcms_requires.on.goneaway, self._on_ovsdbcms_broken)
 
     def _set_central_ips_config(self):
-        addresses =  self.ovsdbcms_requires.bound_addresses()
-        err, _ = call_microovn_command("config","set","ovn.central-ips",
-                                        ",".join(addresses))
+        addresses = self.ovsdbcms_requires.bound_addresses()
+        err, _ = call_microovn_command("config", "set", "ovn.central-ips", ",".join(addresses))
         if err:
             logger.error("calling config set failed with code {0}".format(err))
             return False
         return True
 
-
     def _dataplane_mode(self):
-        if not self._stored.in_cluster or \
-           not self.model.get_relation(OVSDBCMD_RELATION) or \
-           not self.ovsdbcms_requires.remote_ready():
-            logger.debug("not going into dataplane mode, one of these is false in_cluster: {0}, relation_exists: {1}, remote_ready: {2}".format(self._stored.in_cluster,self.model.get_relation(OVSDBCMD_RELATION),self.ovsdbcms_requires.remote_ready()))
+        if (
+            not self._stored.in_cluster
+            or not self.model.get_relation(OVSDBCMD_RELATION)
+            or not self.ovsdbcms_requires.remote_ready()
+        ):
+            logger.debug(
+                "not going into dataplane mode, one of these is false in_cluster: {0}, relation_exists: {1}, remote_ready: {2}".format(
+                    self._stored.in_cluster,
+                    self.model.get_relation(OVSDBCMD_RELATION),
+                    self.ovsdbcms_requires.remote_ready(),
+                )
+            )
             return
 
         self.unit.status = ops.MaintenanceStatus("switching to dataplane mode")
-        err, output = call_microovn_command("disable", "central",
-                              "--allow-disable-last-central")
+        err, output = call_microovn_command("disable", "central", "--allow-disable-last-central")
         if err:
             if "this service is not enabled" in output:
                 logger.debug("central service already disabled")
             else:
-                logger.error("disabling central failed with error code: {0} and output: {1}".format(err,output))
+                logger.error(
+                    "disabling central failed with error code: {0} and output: {1}".format(
+                        err, output
+                    )
+                )
         if self.unit.is_leader():
             if not self._set_central_ips_config():
-                self.unit.status = ops.MaintenanceStatus(
-                    "failed switching to dataplane mode")
+                self.unit.status = ops.MaintenanceStatus("failed switching to dataplane mode")
                 return False
         self.unit.status = ops.ActiveStatus()
-
 
     def _microovn_central_exists(self):
         if not self._stored.in_cluster:
@@ -120,11 +121,17 @@ class MicroovnCharm(ops.CharmBase):
         err, output = call_microovn_command("microovn status")
         if err:
             logger.error("microovn status failed with error code {0}".format(err))
-            raise RuntimeError("microovn status failed with error code {0} and stdout {1}".format(err, output))
+            raise RuntimeError(
+                "microovn status failed with error code {0} and stdout {1}".format(err, output)
+            )
         return "central" in output
 
     def _update_status(self, _: ops.EventBase):
-        if self._stored.in_cluster and not self.model.get_relation(OVSDBCMD_RELATION) and not self._microovn_central_exists():
+        if (
+            self._stored.in_cluster
+            and not self.model.get_relation(OVSDBCMD_RELATION)
+            and not self._microovn_central_exists()
+        ):
             self.unit.status = ops.BlockedStatus(
                 "microovn has no central nodes, this could either be due to a recently broken ovsdb-cms relation or a configuration issue"
             )
@@ -133,8 +140,7 @@ class MicroovnCharm(ops.CharmBase):
         err, output = call_microovn_command("config", "delete", "ovn.central-ips")
         self._update_status(None)
         if err:
-            logger.error(
-                "microovn config delete failed with error code {0}".format(err))
+            logger.error("microovn config delete failed with error code {0}".format(err))
 
     def _on_ovsdbcms_ready(self, _: ops.EventBase):
         self._dataplane_mode()
@@ -155,13 +161,12 @@ class MicroovnCharm(ops.CharmBase):
             return
         combined_cert = str(provider_certificate.certificate) + "\n" + str(provider_certificate.ca)
         combined_input = combined_cert + "\n" + str(private_key)
-        err, output = call_microovn_command("certificates", "set-ca",
-                                "--combined",stdin=combined_input)
+        err, output = call_microovn_command(
+            "certificates", "set-ca", "--combined", stdin=combined_input
+        )
         if err:
-            logger.error(
-                "microovn certificates set-ca failed with error code {0}".format(err))
-            raise RuntimeError(
-                "Updating certificates failed with error code {0}".format(err))
+            logger.error("microovn certificates set-ca failed with error code {0}".format(err))
+            raise RuntimeError("Updating certificates failed with error code {0}".format(err))
         if "New CA certificate: Issued" in output:
             logger.info("CA certificate updated, new certificates issued")
             return True
@@ -172,38 +177,38 @@ class MicroovnCharm(ops.CharmBase):
         self.unit.status = ops.MaintenanceStatus("Installing microovn snap")
         while retries := 3:
             try:
+                subprocess.run(["snap", "wait", "system", "seed.loaded"], check=True)
                 subprocess.run(
-                    ["snap", "wait", "system", "seed.loaded"],
-                    check=True)
-                subprocess.run(
-                    ["snap", "install", "microovn", "--channel", "latest/edge"],
-                    check=True)
+                    ["snap", "install", "microovn", "--channel", "latest/edge"], check=True
+                )
                 break
             except subprocess.CalledProcessError as e:
                 if retries:
                     retries -= 1
                     self.unit.status = ops.MaintenanceStatus(
-                        f"Snap install failed, {retries} retries left")
+                        f"Snap install failed, {retries} retries left"
+                    )
                     time.sleep(1)
                     continue
                 raise e
 
         self.unit.status = ops.MaintenanceStatus("Waiting for microovn ready")
         retries = 0
-        while (code := call_microovn_command("waitready")[0]):
-            retries+=1
-            if retries>3:
-                logger.error(
-                    "microovn waitready failed with error code {0}".format(code))
+        while code := call_microovn_command("waitready")[0]:
+            retries += 1
+            if retries > 3:
+                logger.error("microovn waitready failed with error code {0}".format(code))
                 raise RuntimeError("microovn waitready failed 3 times")
             self.unit.status = ops.MaintenanceStatus(
-                "Microovn waitready failed, retry {0}".format(retries))
+                "Microovn waitready failed, retry {0}".format(retries)
+            )
             time.sleep(1)
 
     def _on_cluster_changed(self, event: ops.RelationChangedEvent):
         if self._stored.in_cluster:
             self.ovsdb_provides.update_relation_data()
             self._dataplane_mode()
+
 
 if __name__ == "__main__":  # pragma: nocover
     ops.main(MicroovnCharm)
--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -8,7 +8,6 @@ import jubilant
 import pytest
 
 
-
 def pytest_addoption(parser: pytest.OptionGroup):
     parser.addoption(
         "--keep-models",
@@ -17,12 +16,13 @@ def pytest_addoption(parser: pytest.OptionGroup):
         help="Keep temporarily created models.",
     )
 
+
 # NOTE: workaround for temp_models with non default cloud in jubilant, to be
 # removed when upstream pr is closed
 @contextlib.contextmanager
-def temp_model(keep: bool = False,
-               controller: str | None = None,
-               cloud: str | None = None) -> Generator[jubilant.Juju, None, None]:
+def temp_model(
+    keep: bool = False, controller: str | None = None, cloud: str | None = None
+) -> Generator[jubilant.Juju, None, None]:
     """Context manager to create a temporary model for running tests in.
 
     This creates a new model with a random name in the format ``jubilant-abcd1234``, and destroys
@@ -36,36 +36,38 @@ def temp_model(keep: bool = False,
         cloud: Name of cloud where the temporary model will be added.
     """
     juju = jubilant.Juju()
-    model = 'jubilant-' + secrets.token_hex(4)  # 4 bytes (8 hex digits) should be plenty
-    juju.add_model(model, controller=controller,cloud=cloud)
+    model = "jubilant-" + secrets.token_hex(4)  # 4 bytes (8 hex digits) should be plenty
+    juju.add_model(model, controller=controller, cloud=cloud)
     try:
         yield juju
     finally:
         if not keep:
             juju.destroy_model(model, destroy_storage=True, force=True)
 
+
 @pytest.fixture
 def juju(request: pytest.FixtureRequest):
-    keep_models = request.config.getoption('--keep-models')
+    keep_models = request.config.getoption("--keep-models")
     # NOTE workaround for juju add-model leading to transaction aborted
     #      error when running in parallel (LP: #2053270).
-    while (retry_count := 3):
+    while retry_count := 3:
         try:
-            with temp_model(keep=keep_models,cloud="localhost") as juju:
+            with temp_model(keep=keep_models, cloud="localhost") as juju:
                 juju.wait_timeout = 15 * 60
-                juju.wait = functools.partial(juju.wait,
-                                              error=jubilant.any_error)
-                juju.model_config({
-                    'image-stream': 'daily',
-                    'enable-os-upgrade': 'false',
-                })
+                juju.wait = functools.partial(juju.wait, error=jubilant.any_error)
+                juju.model_config(
+                    {
+                        "image-stream": "daily",
+                        "enable-os-upgrade": "false",
+                    }
+                )
 
                 yield juju
 
                 if request.session.testsfailed:
                     print(juju.debug_log(limit=1000))
                 break
-        except (jubilant._juju.CLIError) as e:
+        except jubilant._juju.CLIError as e:
             if retry_count and "transaction aborted" in e.stderr:
                 retry_count -= 1
                 time.sleep(0.1)
